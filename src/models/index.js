09af37ac867c509ac00edf26670305aa2183160e
Steve Ngo <stevehngo@hotmail.com>
2022-01-17 02:10:26 -0500

    connect env stuff

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const POSTGRES_URI = process.env.DATABASE_URL || 'sqlite:memory';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
 
 
 const foodSchema = require('./food.schema.js');
 const clothesSchema = require('./clothes.schema.js');
 
 // typical connection string: postgresql://localhost:5432/food
-let db = new Sequelize(POSTGRES_URI);
+let db = new Sequelize(DATABASE_URL);
 
 const FoodModel = foodSchema(db, DataTypes);
 const ClothesModel = clothesSchema(db, DataTypes);
